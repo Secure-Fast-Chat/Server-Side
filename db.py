afbc0aaf5386e5c62175eaaee3f1f6236512e3f8
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-11-09 03:06:26 +0530

    Added signature for storeMessageInDb

--- db.py
+++ db.py
@@ -24,7 +24,7 @@ def checkIfUsernameFree(username: str) -> bool:
         return False
 
 def createUser(username:str, password:str)->None:
-    """Adds a user with the given username and password to the database. Assumes that the checkIfUsernameFree has already been called before. The password is already hashed from the client side. Note that we obviously don't do any salting here
+    """Adds a user with the given username and password to the database. Assumes that the checkIfUsernameFree has already been called before. We hash the password here. TODO: Do we?
 
     :param username: username
     :type username: str
@@ -42,4 +42,25 @@ def createUser(username:str, password:str)->None:
     conn.close()
     return
 
+def loginUsername(username: str, password: str, salt:str)->str:
+    """Checks if a given username is present in the 
+
+    :param username: _description_
+    :type username: str
+    :return: _description_
+    :rtype: str
+    """
+
+
+def storeMessageInDb(sender: str, receiver: str, message: str):
+    """stores the encrypted message in the database, in case it is not possible to send them the message directly
+
+    :param sender: sender username (TODO: Do we keep these encrypted?)
+    :type sender: str
+    :param receiver: receiver username
+    :type receiver: str
+    :param message: the enecrypted message
+    :type message: str
+    """
+    pass
 
